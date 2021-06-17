39ef73ef8904b1f8ea35ecbf7d29de83e8228673
shivam pandey <geetapandey595@gmail.com>
2021-06-18 01:48:06 +0530

    better comments and better code ofcourse

--- AggresiveCows.java
+++ AggresiveCows.java
@@ -1,4 +1,5 @@
 package java_start;
+import java.util.Arrays;
 
 /*
  Params:
@@ -7,10 +8,13 @@ package java_start;
  C: (int) number of cows. Could be between [2,n].
  stalls: (int[]) Array containing list of stalls for placing cows.
  */
+
+//Is the respective optimal stall number possible?? lets check it here below.
 public class AggresiveCows {
 	public static int C = 3;
 
 	public static boolean canPlaceCows(int minimumDist, int[] stalls) {
+		//atlest 1 cow will be placed, Ofcourse.
 		int cows = 1;
 		int pos = stalls[0];
 		
@@ -26,6 +30,8 @@ public class AggresiveCows {
 		
 		return false;
 	}
+	
+	//Finding optimal stall using monotonic behaviour of BS
 	public static int binarySearch(int []stalls){
 		int low = 0;
 		int high = stalls.length - 1;
@@ -48,6 +54,11 @@ public class AggresiveCows {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int stalls[] = {1,2,4,8,9};
+		
+		//A sorted is a must to use monotonic behaviour of Binary search.
+		Arrays.sort(stalls);
+		
+		//returns the maximum minimum distance of placing all cows/wolfs/elephants/dragons!
 		int resultantStall = binarySearch(stalls);
 		
 		System.out.println("Maximum minimum distance-> "+resultantStall);
